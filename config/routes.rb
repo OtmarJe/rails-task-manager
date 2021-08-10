501b1446bf9db43c90d53542db9a08fec2b2b82c
Otmar Jenner <o.jenner@gmx.de>
2021-08-10 16:42:52 +0200

    finished Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # a list of all tasks
+  get '/tasks', to: 'tasks#index', as: :tasks
+  # a new inputform
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  # show one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # submit new task
+  post '/tasks', to: 'tasks#create'
+  # disply task to edit
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # submit edited inputform
+  patch '/tasks/:id', to: 'tasks#update'
+  # destroy a task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
